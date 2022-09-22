1b52a54b09d74ef06f36f668383d7c813e0c77a0
vectorxu <vectorxu@tencent.com>
2022-09-22 20:58:12 +0800

    feat: 文档更新

--- static/weapp/js/71.397c13bd.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[71],{"103":function(n,o,t){"use strict";t.r(o),t.d(o,"taro_block_core",(function(){return r}));var c=t(23),r=function(){function Block(n){Object(c.g)(this,n)}return Block.prototype.render=function(){return Object(c.f)(c.a,null)},Block}()}}]);
